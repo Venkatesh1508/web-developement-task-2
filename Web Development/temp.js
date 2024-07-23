defa4a3a2fb9e0442158a43bc09db0b42fbd2275
Venkatesh1508 <118344871+Venkatesh1508@users.noreply.github.com>
2024-07-23 22:53:20 +0530

    Delete Web Development/temp.js

--- Web Development/temp.js
+++
@@ -1,5 +0,0 @@
-function convertToFahrenheit() {
-    var fahrenheit = document.getElementById("fahrenheit").value;
-    var celsius = (fahrenheit - 32) * (5/9);
-    document.getElementById("result").innerText = `${fahrenheit} Fahrenheit is ${celsius.toFixed(2)} Celsius`;
-}
